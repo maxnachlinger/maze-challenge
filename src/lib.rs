26a67dfcbad3b4d1b15e6dfa6257063d5feef548
Max Nachlinger <max.nachlinger@gmail.com>
2021-01-24 22:50:04 -0800

    More maze work

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,6 @@ mod check_solution;
 mod generate_maze;
 mod util;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn generate_maze(num_rows: i32, num_cols: i32) -> JsValue {
     let maze = generate_rectangular_maze(num_rows, num_cols);
