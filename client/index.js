930808a25f0b06234b2a826b5a9b56fe1c4f9db9
Max Nachlinger <max.nachlinger@gmail.com>
2022-04-14 17:21:03 -0700

    Refactor

--- client/index.js
+++ client/index.js
@@ -28,8 +28,6 @@ const drawState = ({
     mazeDefinitionTextarea,
     solutionResultsDiv,
     solutionTextarea,
-    testSolutionButton,
-    generateMazeLink,
   },
 }) => {
   if (animationHandle) {
