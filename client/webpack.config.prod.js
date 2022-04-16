d708259c9697307b0c5ff7841293fb6b8301d942
Max Nachlinger <max.nachlinger@gmail.com>
2022-04-16 08:56:39 -0700

    Refactor

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./bootstrap.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../docs"),
     filename: "bootstrap.js",
   },
   mode: "production",
